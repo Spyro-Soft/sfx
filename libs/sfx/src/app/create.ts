759d21d95541fa24daa7c738e69c7128436093a5
Chewbacca <chewbacca@millennium.falcon>
2024-02-01 16:56:20 +0100

    fix: test

--- libs/sfx/src/app/create.ts
+++ libs/sfx/src/app/create.ts
@@ -1,4 +1,4 @@
-import { exec, spawn } from 'child_process';
+import { exec, spawn, spawnSync } from 'child_process';
 import { createWorkspace } from 'create-nx-workspace';
 
 import { Config } from '../config/config.js';
@@ -100,27 +100,32 @@ function installAdditionalLibraries(baseOptions: IBaseOptions, answers: IFormOpt
     `npx nx g @spyrosoft/spyro-plugin-manager:setup-all --appName=${baseOptions.appName} --framework=${baseOptions.framework} --ciCd=${answers.repositoryPlatforms} --extend=false --interactive=false`,
   ];
   const toExecute = commands.reduce((a, b) => a + ' && ' + b);
-  const command = spawn(toExecute, { shell: true, stdio: 'overlapped'});
-  command.on('close', (code) => {
-    console.log(code)
-    if (code === 0) {
-      // commitAllChanges();
-      displayMessage(['Your application is ready for development', `Thank you for using ${Config.cliName}!`]);
-      if (process.platform === 'win32') {
-        process.exit();
-      }
-    }
-    // handleError(new InstallationError());
-  });
+  const command = spawnSync(toExecute, { shell: true, stdio: 'overlapped'});
+  console.log('output', command.output)
+  console.log('stdout', command.stdout)
+  console.log('error', command.error)
+  console.log('status', command.status)
+  console.log('stderr', command.stderr)
+  // command.on('close', (code) => {
+  //   console.log(code)
+  //   if (code === 0) {
+  //     // commitAllChanges();
+  //     displayMessage(['Your application is ready for development', `Thank you for using ${Config.cliName}!`]);
+  //     if (process.platform === 'win32') {
+  //       process.exit();
+  //     }
+  //   }
+  //   // handleError(new InstallationError());
+  // });
 
-  command.stderr.on('data', (error) => {
-    console.log(error.toString());
-    if (error.includes(`.git can't be found `)) {
-      handleError(new GitExistsInstallationError());
-    }
-  });
+  // command.stderr.on('data', (error) => {
+  //   console.log(error.toString());
+  //   if (error.includes(`.git can't be found `)) {
+  //     handleError(new GitExistsInstallationError());
+  //   }
+  // });
 
-    command.stdout.on('data', (data) => {
-      displayMessage(data.toString());
-    });
+  //   command.stdout.on('data', (data) => {
+  //     displayMessage(data.toString());
+  //   });
 }
