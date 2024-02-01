24dee3c166177f16269fff2300fe206ff9e847ba
Chewbacca <chewbacca@millennium.falcon>
2024-02-01 17:24:11 +0100

    fix: test

--- libs/sfx/src/app/create.ts
+++ libs/sfx/src/app/create.ts
@@ -100,12 +100,18 @@ function installAdditionalLibraries(baseOptions: IBaseOptions, answers: IFormOpt
     `npx nx g @spyrosoft/spyro-plugin-manager:setup-all --appName=${baseOptions.appName} --framework=${baseOptions.framework} --ciCd=${answers.repositoryPlatforms} --extend=false --interactive=false`,
   ];
   const toExecute = commands.reduce((a, b) => a + ' && ' + b);
-  const command = spawnSync(toExecute);
+  const command = spawnSync('npm i @spyrosoft/spyro-plugin-manager --save-dev --save-exact');
+  const command1 = spawnSync(`npx nx g @spyrosoft/spyro-plugin-manager:setup-all --appName=${baseOptions.appName} --framework=${baseOptions.framework} --ciCd=${answers.repositoryPlatforms} --extend=false --interactive=false`);
   console.log('output', command.output)
   console.log('stdout', command.stdout)
   console.log('error', command.error)
   console.log('status', command.status)
   console.log('stderr', command.stderr)
+  console.log('output', command1.output)
+  console.log('stdout', command1.stdout)
+  console.log('error', command1.error)
+  console.log('status', command1.status)
+  console.log('stderr', command1.stderr)
   // command.on('close', (code) => {
   //   console.log(code)
   //   if (code === 0) {
